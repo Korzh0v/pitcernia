50e812386efdafcafb267c99cdd91f047230b979
biru357 <bartosz.birecki357@gmail.com>
2025-09-18 12:17:56 +0200

    Add files via upload
    do dokonczenia

---
+++ zamowienia.php
@@ -0,0 +1,43 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "pitceria");
+if ($conn->connect_error) {
+    die("Błąd połączenia");
+}
+
+$sql = "SELECT 
+            klient.nazwa AS klient_nazwa, pizza.nazwa AS pizza_nazwa, pizza.cena, zamowienia.ilosc, zamowienia.id, zamowienia.id_klient, zamowienia.id_pizza FROM zamowienia JOIN klient ON klient.id = zamowienia.id_klient JOIN pizza ON pizza.id = zamowienia.id_pizza";
+$result = $conn->query($sql);
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>pitceria</title>
+</head>
+<body>
+<hr>
+<h2>AKTYWNE ZAMÓWIENIA</h2>
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Klient</th>
+        <th>Pizza</th>
+        <th>Ilość</th>
+        <th>Cena pizzy</th>
+        <th>Łączna cena</th>
+    </tr>
+    <?php while ($row = $result->fetch_assoc()) { ?>
+    <tr>
+        <td><?= $row['id'] ?></td>
+        <td><?= $row['klient_nazwa'] ?></td>
+        <td><?= $row['pizza_nazwa'] ?></td>
+        <td><?= $row['ilosc'] ?></td>
+        <td><?= $row['cena'] ?> zł</td>
+        <td><?= $row['ilosc'] * $row['cena'] ?> zł</td>
+    </tr>
+    <?php } ?>
+</table>
+<?php $conn->close(); ?>
+</body>
+</html>
